46b04561028d3c815e94dfb2e88ca10ee4500ca8
Lokesh Kumar R <lokeshkumarr2905@gmail.com>
2025-12-28 16:24:57 +0530

    fix: remove unwanted imports

--- backend/configs/config.py
+++ backend/configs/config.py
@@ -13,21 +13,9 @@ DEFAULT_CANNED_MESSAGE = os.getenv(
 )
 
 # Model parameters
-DEFAULT_MODEL_TEMPERATURE = float(os.getenv("DEFAULT_MODEL_TEMPERATURE", "0.1"))
-DEFAULT_MODEL_TOP_P = float(os.getenv("DEFAULT_MODEL_TOP_P", "1.0"))
-DEFAULT_MODEL_MAX_TOKENS = int(os.getenv("DEFAULT_MODEL_MAX_TOKENS", "1024"))
-DEFAULT_MODEL_IS_STREAMING = os.getenv("DEFAULT_MODEL_IS_STREAMING", "false").lower() == "true"
 
 KB_RETRIEVE_NUM_OF_RESULTS = int(os.getenv("KB_RETRIEVE_NUM_OF_RESULTS", "5"))
-
-class LLMModelConfig(BaseModel):
-  model_id: str
-  model_temperature: float | None = None
-  model_max_tokens: int | None = None
-  model_top_p: float | None = None
-  model_streaming: bool | None = None
 
 class AnswerRequestPayload(BaseModel):
   text: str | None = None
   agent_session_id: str | None = None
-  llm_model_configs: List[LLMModelConfig] | None = None
